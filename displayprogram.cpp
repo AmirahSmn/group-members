d02ae28c78c29f7b80c9a778910d87e7772bfcfa
AmirahSmn <37636601+AmirahSmn@users.noreply.github.com>
2019-11-29 08:02:13 +0300

    name added

--- displayprogram.cpp
+++ displayprogram.cpp
@@ -5,6 +5,8 @@ int main()
     cout << "Group 8!";
     cout<<"Name:- Amirah Semman endln";
         cout<<"Name:- Tadiyos Seyoum endln";
+        cout<<"Name:- yared haftamu endln";
+    
    
 
     return 0;
